9e083ac7cea88a3b61e21dba95b1754feef556ad
Ahmed Gad <admin@motwreen.com>
2017-07-23 05:04:52 +0200

    fix folder existance

--- grap.php
+++ grap.php
@@ -4,6 +4,10 @@ $url 	= $_GET['image'];
 $str 	= parse_url($url);
 $path 	= explode('/', $str['path']);
 
+if (!file_exists('images')) {
+    mkdir('images', 0777, true);
+}
+
 copy($url, 'images/'.end($path));
 $_SESSION['message'] = 'Downloaded Success';
 return header('location: icons.php?keyword='.$_GET['back'].'&page='.$_GET['page']);
